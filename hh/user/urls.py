c7e100df429b373097d7bd511bc93adce81dc8d8
usm0n0v1ch <dilmurodtagandurdiyev@gmail.com>
2024-09-10 02:13:04 +0500

    urls.py

--- hh/user/urls.py
+++ hh/user/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from user import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.log_out, name='logout'),
 ]
